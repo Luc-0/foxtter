e99ed517942b857fdde65bc2b9503c95ce95d949
Luc-0 <luc0x00@gmail.com>
2021-03-09 20:20:34 -0300

    Add home route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import PublicRoute from './helpers/PublicRoute';
+import PrivateRoute from './helpers/PrivateRoute';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Login from './pages/Login';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import { Container, FlexContainer } from './components/StyledComponents';
 
 function App() {
   return (
@@ -15,8 +18,23 @@ function App() {
           path="/login"
           component={Login}
         />
-        {/* Authenticated */}
-        {true ? <Navbar /> : null}
+        <FlexContainer jc="flex-start" ai="flex-start">
+          {/* Authenticated */}
+          {true ? <Navbar /> : null}
+          <main>
+            <FlexContainer>
+              <PrivateRoute
+                authenticated={true}
+                exact
+                path="/home"
+                component={Home}
+              />
+              <Container bgc="rgb(235, 238, 240)" wt="100%">
+                Hold...
+              </Container>
+            </FlexContainer>
+          </main>
+        </FlexContainer>
       </Switch>
     </Router>
   );
--- src/components/StyledComponents.js
+++ src/components/StyledComponents.js
@@ -125,7 +125,7 @@ const HomeContainer = styled(FlexContainer)`
   width: 100%;
   height: 100%;
   min-height: 100vh;
-  max-width: 600px;
+  min-width: 600px;
   border-right: 1px solid ${COLORS.borderColor};
 `;
 
