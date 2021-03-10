0cd12a94c36360b54e13b4b2696919caf4a02de5
Luc-0 <luc0x00@gmail.com>
2021-03-10 16:53:06 -0300

    Add app routers

--- src/App.js
+++ src/App.js
@@ -5,37 +5,65 @@ import Landing from './pages/Landing';
 import Login from './pages/Login';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Profile from './pages/Profile';
 import { Container, FlexContainer } from './components/StyledComponents';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <PublicRoute authenticated={true} exact path="/" component={Landing} />
+        <PublicRoute authenticated={false} exact path="/" component={Landing} />
         <PublicRoute
-          authenticated={true}
+          authenticated={false}
           exact
           path="/login"
           component={Login}
         />
-        <FlexContainer jc="flex-start" ai="flex-start">
-          {/* Authenticated */}
-          {true ? <Navbar /> : null}
-          <main>
-            <FlexContainer>
+      </Switch>
+
+      <FlexContainer jc="flex-start" ai="flex-start">
+        {/* Authenticated */}
+        {true ? <Navbar /> : null}
+        <main>
+          <FlexContainer>
+            <Switch>
               <PrivateRoute
                 authenticated={true}
                 exact
                 path="/home"
                 component={Home}
               />
-              <Container bgc="rgb(235, 238, 240)" wt="100%">
-                Hold...
-              </Container>
-            </FlexContainer>
-          </main>
-        </FlexContainer>
-      </Switch>
+              <PrivateRoute
+                authenticated={true}
+                exact
+                path="/notifications"
+                component={() => <div>Notifications</div>}
+              />
+              <PrivateRoute
+                authenticated={true}
+                exact
+                path="/messages"
+                component={() => <div>Messages</div>}
+              />
+              <PrivateRoute
+                authenticated={true}
+                exact
+                path="/connect_people"
+                component={() => <div>Connect people</div>}
+              />
+              <PrivateRoute
+                authenticated={true}
+                exact
+                path="/:id"
+                component={Profile}
+              />
+            </Switch>
+            <Container bgc="rgb(235, 238, 240)" wt="100%">
+              Hold...
+            </Container>
+          </FlexContainer>
+        </main>
+      </FlexContainer>
     </Router>
   );
 }
