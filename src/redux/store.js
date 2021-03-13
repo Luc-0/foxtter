e9c0c2744015bd1e0bc563c6fba9cac57a706661
Luc-0 <luc0x00@gmail.com>
2021-03-13 20:45:52 -0300

    Add initial redux

---
+++ src/redux/store.js
@@ -0,0 +1,24 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './reducers/reducer';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// Logger middleware
+const logger = (store) => {
+  return (next) => {
+    return (action) => {
+      console.log('[Middleware] Dispatching', action);
+      const result = next(action);
+      console.log('[Middleware] Next state', store.getState());
+      return result;
+    };
+  };
+};
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger, thunk))
+);
+
+export default store;
